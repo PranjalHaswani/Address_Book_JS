479eabf2a0f8380595f2e3b7e83955ff738fee92
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-03-05 11:42:45 +0530

    [Pranjal_Haswani]add. Ability to find existing contact person using their name and edit it.

--- src/index.js
+++ src/index.js
@@ -9,16 +9,21 @@ try {
         "Pranjal", "Haswani", "Bhopal", "India", "Chhatisgarh", "100001", "9345678654", "pranjalhaswani00@gmail.com"
     );
     console.log(addressBook.addContact(contact1));
+    
     const contact2 = new Contact(
         "Sunny","Haswani","Bhopal","India", "Chhatisgarh", "100001", "6234567801","sunnyhaswani00@gmail.com");
     console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts", addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+
+    console.log(addressBook.editContact("Sunny", { address: "Rewa", city: "London", phone: "6024764565" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
+
 } catch (error) {
     console.error("Error:", error.message);
 }
 try {
     const invalidContact = new Contact(
-        "Pra", "Haswani", "abcd", "Indi", "Chhat", "100101", "6323458452", "invalid-email"
+        "Pra", "Haswani", "abcd", "Indi", "Chhat", "100101", "6323458452", "sanjayhaswani012@gmail.com"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
