c5ff5e72c36cc54823c2131d5ece7b38778415ad
Pranjal Haswani <pranjalhaswani00@gmail.com>
2025-03-05 12:59:58 +0530

    [Pranjal_Haswani]add. Sort the entries in the address book by city, state or zip.

--- src/index.js
+++ src/index.js
@@ -12,24 +12,44 @@ try {
     
     const contact2 = new Contact(
         "Sunny","Haswani","Bhopal","India", "Chhatisgarh", "100001", "6234567801","sunnyhaswani00@gmail.com");
+
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
 
     console.log(addressBook.editContact("Sunny", { address: "Rewa", city: "London", phone: "6024764565" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
+
     console.log(addressBook.deleteContact("Sunny"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
+
     console.log("Total Contacts:", addressBook.countContacts());
+
     const duplicateContact=new Contact("Rimi", "Haswani", "Bhopal", "India", "Chhatisgarh", "100001", "9345678654", "pranjalhaswani00@gmail.com");
     console.log(addressBook.addContact(duplicateContact));
+
     console.log("Contacts in India:");
     console.log(addressBook.searchByCityOrState("India"));
+
     console.log("Viewing Persons by City or State:");
     console.log(addressBook.viewPersonsByCityOrState());
+
     console.log("Count of Contacts by City and State:");
     console.log(addressBook.countByCityOrState());
+
     console.log("\nContacts Sorted Alphabetically:");
     console.log(addressBook.sortContactsByName().map(contact => contact.toString()).join("\n"));
+
+    console.log("\nContacts Sorted by Name:");
+    console.log(addressBook.sortContactsByName().map(contact => contact.toString()).join("\n"));
+
+    console.log("\nContacts Sorted by City:");
+    console.log(addressBook.sortContactsByCity().map(contact => contact.toString()).join("\n"));
+
+    console.log("\nContacts Sorted by State:");
+    console.log(addressBook.sortContactsByState().map(contact => contact.toString()).join("\n"));
+
+    console.log("\nContacts Sorted by Zip:");
+    console.log(addressBook.sortContactsByZip().map(contact => contact.toString()).join("\n"));
 
 } catch (error) {
     console.error("Error:", error.message);
